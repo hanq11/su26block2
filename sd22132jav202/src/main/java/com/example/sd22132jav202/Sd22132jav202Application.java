96434e9a5e45be9f37507da6aca6a6d98bfa3626
hanq11 <>
2026-06-29 09:02:24 +0700

    update

---
+++ sd22132jav202/src/main/java/com/example/sd22132jav202/Sd22132jav202Application.java
@@ -0,0 +1,13 @@
+package com.example.sd22132jav202;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sd22132jav202Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sd22132jav202Application.class, args);
+    }
+
+}
