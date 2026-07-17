4254a6df29745a8a9b979d564b088ed002f3d78c
hanq11 <>
2026-07-17 08:46:13 +0700

    update

--- sd22132jav202/src/main/java/com/example/sd22132jav202/buoi4/controller/SinhVienController.java
+++ sd22132jav202/src/main/java/com/example/sd22132jav202/buoi4/controller/SinhVienController.java
@@ -1,11 +1,11 @@
 package com.example.sd22132jav202.buoi4.controller;
 
+import com.example.sd22132jav202.buoi4.entity.SinhVien;
 import com.example.sd22132jav202.buoi4.service.SinhVienService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/buoi4")
@@ -17,5 +17,29 @@ public class SinhVienController {
     public String hienThi(Model model) {
         model.addAttribute("listSinhVien", sinhVienService.getAll());
         return "/buoi4/hien-thi";
+    }
+
+    @PostMapping("/them")
+    public String themSinhVien(SinhVien sinhVien) {
+        sinhVienService.themSinhVien(sinhVien);
+        return "redirect:/buoi4/hien-thi";
+    }
+
+    @GetMapping("/view-update/{id}")
+    public String viewUpdate(@PathVariable("id") Integer id, Model model) {
+        model.addAttribute("sinhVien", sinhVienService.getById(id));
+        return "/buoi4/view-update";
+    }
+
+    @PostMapping("/sua")
+    public String suaSinhVien(SinhVien sinhVien) {
+        sinhVienService.suaSinhVien(sinhVien);
+        return "redirect:/buoi4/hien-thi";
+    }
+
+    @GetMapping("/xoa")
+    public String xoaSinhVien(@RequestParam("id") Integer id) {
+        sinhVienService.xoaSinhVien(id);
+        return "redirect:/buoi4/hien-thi";
     }
 }
