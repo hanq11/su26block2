620dfa2a2571523d5d02b18d443a9500ec9194d6
hanq11 <>
2026-07-27 10:48:20 +0700

    update

--- jav20201/src/main/java/com/example/jav20201/buoi9/controller/Buoi9Controller.java
+++ jav20201/src/main/java/com/example/jav20201/buoi9/controller/Buoi9Controller.java
@@ -1,12 +1,16 @@
 package com.example.jav20201.buoi9.controller;
 
+import com.example.jav20201.buoi9.entity.DanhMuc;
 import com.example.jav20201.buoi9.entity.SanPham;
+import com.example.jav20201.buoi9.repository.DanhMucRepository;
 import com.example.jav20201.buoi9.repository.SanPhamRepository;
+import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/buoi9")
@@ -14,9 +18,41 @@ public class Buoi9Controller {
     @Autowired
     SanPhamRepository sanPhamRepository;
 
+    @Autowired
+    DanhMucRepository danhMucRepository;
+
     @GetMapping("/hien-thi")
     public String hienThi(Model model) {
         model.addAttribute("listSanPham", sanPhamRepository.findAll());
+        model.addAttribute("listDanhMuc", danhMucRepository.findAll());
         return "/buoi9/hien-thi";
+    }
+
+    // getById: sanPhamRepository.findById(id).get()
+    // them, sua: sanPhamRepository.save()
+    // xoa: sanPhamRepository.deleteById(id)
+
+    @PostMapping("/them")
+    public String them(SanPham sanPham) {
+        sanPhamRepository.save(sanPham);
+        return "redirect:/buoi9/hien-thi";
+    }
+//
+//    @ModelAttribute("listDanhMuc")
+//    public List<DanhMuc> getListDanhMuc() {
+//        return danhMucRepository.findAll();
+//    }
+
+    @GetMapping("/view-update/{id}")
+    public String viewUpdate(@PathVariable("id") Integer id, Model model) {
+        model.addAttribute("sp", sanPhamRepository.findById(id).get());
+        model.addAttribute("listDanhMuc", danhMucRepository.findAll());
+        return "/buoi9/view-update";
+    }
+
+    @PostMapping("/sua")
+    public String sua(SanPham sanPham) {
+        sanPhamRepository.save(sanPham);
+        return "redirect:/buoi9/hien-thi";
     }
 }
